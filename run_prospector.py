b89983182231feb45bd9eafe2e47ce6726e90f19
lockepatton <locke.patton@cfa.harvard.edu>
2020-04-15 20:12:48 -0400

    added slurm files; modifications for run_prospector.py on new odyssey setup

--- run_prospector.py
+++ run_prospector.py
@@ -1,10 +1,13 @@
 import sys
+import os
 # from prospectTools import *
 
 # INPUTS 1
 # defining galaxies to be fit in this slurm run
-Galaxies_To_Fit = ['12dam_SDSSPS1','12dam_all']
+# Galaxies_To_Fit = ['12dam_SDSSPS1','12dam_all']
 # Galaxies_To_Fit = ['PS19qp']
+# Galaxies_To_Fit = ['PTF10hgi', 'SN 2010kd', 'PTF12dam', 'SN 2007bi', 'SN 2011ke', 'SN 2012il', 'PTF11rks', 'SN 2010gx', 'SN 2011kf', 'PTF09cnd', 'SN 2005ap', 'MLS121104:021643+204009', 'PTF09cwl', 'SN 2006oz', 'PTF09atu', 'PS1-12bqf', 'PS1-11ap', 'PS1-10bzj', 'PS1-12zn', 'PS1-11bdn', 'PS1-13gt', 'PS1-10awh', 'PS1-10ky', 'PS1-11aib', 'SCP 06F6', 'PS1-10pm', 'PS1-11tt', 'PS1-10afx', 'PS1-11afv', 'PS1-11bam', 'PS1-12bmy']
+Galaxies_To_Fit = ['09as  ', '09uy  ', '09atu ', '09cnd ', '09cwl ', '10bfz ', '10bjp ', '10cwr ', '10fel ', '10heh ', '10hgi ', '10jwd ', '10nmn ', '10qaf ', '10qwu ', '10scc ', '10tpz ', '10uhf ', '10vqv ', '10vwg ', '10yyc ', '10aagc', '11dij ', '11dsf ', '11hrq ', '11rks ', '12dam ', '12epg ', '12gwu ', '12mue ', '12mkp ', '12mxx ']
 
 
 # Command Line TaskID
@@ -25,6 +28,7 @@ outfile = os.path.join(APPS,'prospector_alpha/results/slsn/' + Galaxy)
 datfile = os.path.join(APPS,'prospector_alpha/data/SDSSPS1_vs_all_12dam.json')
 # datfile = os.path.join(APPS,'prospector_alpha/data/newtargets.json')
 # datfile = os.path.join(APPS,'prospector_alpha/data/J-ApJ-830-13.json')
+datfile = os.path.join(APPS,'prospector_alpha/data/2020_04_12_J-ApJ-830-13.json')
 
 dynasty_cmd = os.path.join(APPS,'prospector_dynesty.py')
 param_file = os.path.join(APPS,'new_sne_params.py')
